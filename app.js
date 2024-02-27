d8049e80adcafb363b1b1054a4a838e1ec65b572
AkashFullStackDev <akashfullstackdev@gmail.com>
2024-02-27 20:57:09 +0530

    second commit

--- app.js
+++ app.js
@@ -11,9 +11,6 @@ const verifyOrder = require("./helpers/verifyOrder");
 const addProducts = require("./helpers/addProducts");
 
 const PORT = 8080 || process.env.PORT;
-
-
-
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
@@ -24,35 +21,27 @@ app.get("/", (req, res) => {
 });
 
 // ************** Signup API ********************
-
 app.post("/api/signup", signup);
 
 // ************** Login API ********************
-
 app.post("/api/login", login);
 
 // ************** Add Products API ********************
-
 app.get("/api/addproducts", addProducts)
 
 // ******************Getting Products API******************
-
 app.get("/api/products", getProducts);
 
 // *********************Update User Details***************
-
 app.post("/api/updateuser", verifyToken, updateUser);
 
 // ******************** RAZORPAY INTEGRATION ******************************
-
 app.post("/api/order", placeOrder);
 
 // **********************Order Validation ****************
-
 app.post("/api/order/validate", verifyOrder);
 
 // ************** Running the server ********************
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- helpers/getProducts.js
+++ helpers/getProducts.js
@@ -1,26 +1,43 @@
 const db = require("../models/index");
 
 const getProducts = async (req, res) => {
-  const {page, limit, name} = req.query;
-  
-  const skip = (page - 1) * limit;
-  const queryName = {$regex: name, $options: "i"}
-  console.log(queryName)
+  const { page, limit, name, category, subCategory } = req.query;
+  const queryCategory = {};
 
-    try {
-      const products = await db.Products.find(name?{name: queryName}:{}).skip(skip).limit(limit);
-  
-      if (products) {
-        console.log("Data sent successfully!");
-        res.status(200).json(products);
-      } else {
-        console.log("Data not found!");
-        res.status(500).json({ msg: "Data not found!" });
-      }
-    } catch(err) {
-      console.log(`Database find error! ${err}`);
-      res.status(500).json({ msg: "Database find error!" });
-    }
+  if (category) {
+    queryCategory.category = category;
+  }
+  if (subCategory) {
+    queryCategory.subcategory = subCategory;
+  }
+  if (name) {
+    queryCategory.name = { $regex: name, $options: "i" };
+  }
+  if (category == "all") {
+    delete queryCategory.category;
+  }
+  if (subCategory == "all") {
+    delete queryCategory.subcategory;
   }
 
-  module.exports = getProducts;
+  const skip = (page - 1) * limit;
+
+  try {
+    const products = await db.Products.find(name ? queryCategory : {})
+      .skip(skip)
+      .limit(limit);
+
+    if (products) {
+      console.log("Data sent successfully!");
+      res.status(200).json(products);
+    } else {
+      console.log("Data not found!");
+      res.status(500).json({ msg: "Data not found!" });
+    }
+  } catch (err) {
+    console.log(`Database find error! ${err}`);
+    res.status(500).json({ msg: "Database find error!" });
+  }
+};
+
+module.exports = getProducts;
